49c7423039a1cecde83adcdbf77b97b583e51b0c
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2026-01-12 05:53:46 -0600

    Replace simple shifting with XOR and shifting in encryption algorithm

--- simple_encryption_algorithm.py
+++ simple_encryption_algorithm.py
@@ -5,7 +5,7 @@ def main():
     # Encrypt the password
     encrypted_password = password_manager.encrypt()
     print(f"Encrypted password: {encrypted_password}")
-
+    print()
     # Decrypt the password
     decrypted_password = password_manager.decrypt(encrypted_password)
     print(f"Decrypted password: {decrypted_password}")
@@ -16,12 +16,22 @@ class SimpleEncryption:
         self.size = len(password)
 
     def encrypt(self):
-        encrypted = ''.join(chr(ord(char) + self.size) for char in self.password)
-        return encrypted
+        # encrypted = ''.join(chr(ord(char)^self.size + self.size) for char in self.password)
+        encrypted = []
+        for i in range(len(self.password)):
+            encrypted_char = chr((ord(self.password[i]) + i) ^ (i + self.size))
+            encrypted.append(encrypted_char)
+            print(f"Encrypting char '{self.password[i]} : {ord(self.password[i])}' to '{encrypted_char} : {ord(encrypted_char)}'")
+        return "".join(encrypted)
 
     def decrypt(self, encrypted_password):
-        decrypted = ''.join(chr(ord(char) - self.size) for char in encrypted_password)
-        return decrypted
-    
+        # decrypted = ''.join(chr((ord(char)^i) - i) for char in encrypted_password)
+        decrypted = []
+        for i in range(len(encrypted_password)):
+            decrypted_char = chr((ord(encrypted_password[i]) ^ (i + self.size)) - i)
+            decrypted.append(decrypted_char)
+            print(f"Decrypting char '{encrypted_password[i]} : {ord(encrypted_password[i])}' to '{decrypted_char} : {ord(decrypted_char)}'")
+        return "".join(decrypted)
+
 if __name__ == "__main__":
     main()
